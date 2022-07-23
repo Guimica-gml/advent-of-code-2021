b1d9304e65b632bc1a0e89187e46c69b26e4b6d4
Guimica-gml <nicholasalemida567@gmail.com>
2022-07-23 12:14:35 -0300

    Inlining ifs in main.rs

--- src/main.rs
+++ src/main.rs
@@ -22,30 +22,14 @@ fn main() {
         }
     };
 
-    if day == "1" {
-        day_1::main();
-    }
-    else if day == "2" {
-        day_2::main();
-    }
-    else if day == "3" {
-        day_3::main();
-    }
-    else if day == "4" {
-        day_4::main();
-    }
-    else if day == "5" {
-        day_5::main();
-    }
-    else if day == "6" {
-        day_6::main();
-    }
-    else if day == "7" {
-        day_7::main();
-    }
-    else if day == "8" {
-        day_8::main();
-    }
+    if      day == "1" { day_1::main(); }
+    else if day == "2" { day_2::main(); }
+    else if day == "3" { day_3::main(); }
+    else if day == "4" { day_4::main(); }
+    else if day == "5" { day_5::main(); }
+    else if day == "6" { day_6::main(); }
+    else if day == "7" { day_7::main(); }
+    else if day == "8" { day_8::main(); }
     else {
         println!("Error: Command not recognized");
         process::exit(1);
